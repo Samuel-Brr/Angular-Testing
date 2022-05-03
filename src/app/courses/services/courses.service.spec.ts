d236ac0c4bf3a030877e17f2769c51c0b17030c3
Samuel-Brr <samuel.barre972@gmail.com>
2022-05-03 10:50:51 +0200

    Angular HTTP Testing - Checking HTTP Request Parameters

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service"
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
 import { catchError, tap } from "rxjs/operators";
-import { COURSES } from "../../../../server/db-data";
+import { COURSES, findLessonsForCourse } from "../../../../server/db-data";
 import { Course } from "../model/course";
 import { throwError } from "rxjs";
 import { HttpErrorResponse } from "@angular/common/http";
@@ -126,6 +126,36 @@ describe('CoursesService', () => {
       expect(req.request.method).toEqual('PUT');
 
       req.flush('Save course failed', {status: 500, statusText: 'Internal Server Error'})
+  })
+
+  it('Should find a list of lessons', () => {
+
+    coursesService.findLessons(12)
+      .pipe(
+        tap(lessons => {
+
+          expect(lessons).toBeTruthy();
+
+          expect(lessons.length).toBe(3);
+
+        })
+      )
+      .subscribe()
+
+    const req = httpTestingController.expectOne(req =>
+      req.url == '/api/lessons'); //Si il y a des wildcards et des queries params dans l'url de la requete utiliser cette syntax
+
+    expect(req.request.method).toEqual('GET');
+
+    expect(req.request.params.get('courseId')).toEqual('12');
+    expect(req.request.params.get('filter')).toEqual('');
+    expect(req.request.params.get('sortOrder')).toEqual('asc');
+    expect(req.request.params.get('pageNumber')).toEqual('0');
+    expect(req.request.params.get('pageSize')).toEqual('3');
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0,3)
+    })
   })
 
   afterEach(() => {
