95aa01cfd825e5308615be0ab5df69dc56c73d43
Samuel-Brr <samuel.barre972@gmail.com>
2022-05-03 06:53:53 +0200

    Preventing Unintended HTTP Requests using the HTTP Testing Controller

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service"
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
+import { tap } from "rxjs/operators";
+import { COURSES } from "../../../../server/db-data";
 
 describe('CoursesService', () => {
 
@@ -11,7 +13,7 @@ describe('CoursesService', () => {
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
+providers: [
         CoursesService
       ]
     })
@@ -19,9 +21,59 @@ describe('CoursesService', () => {
     coursesService = TestBed.inject(CoursesService)
     httpTestingController = TestBed.inject(HttpTestingController)
 
-  })
+  });
 
   it('Should retrieve all courses', () => {
 
-  })
-})
+    coursesService.findAllCourses()
+      .pipe(
+        tap(courses => {
+
+          expect(courses).toBeTruthy('No courses returned');
+
+          expect(courses.length).toBe(12, 'Incorrect number of courses');
+
+          const course = courses.find(course => course.id == 12);
+
+          expect(course.titles.description).toBe("Angular Testing Course")
+
+        })
+      )
+      .subscribe();
+
+    const req = httpTestingController.expectOne('/api/courses');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({payload: Object.values(COURSES)});
+
+  });
+
+  it('Should retrieve a course by id', () => {
+
+    coursesService.findCourseById(12)
+      .pipe(
+        tap(course => {
+
+          expect(course).toBeTruthy('No courses returned');
+
+          expect(course.id).toBe(12, 'Incorrect course');
+
+        })
+      )
+      .subscribe();
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(COURSES[12]);
+  });
+
+  afterEach(() => {
+
+    httpTestingController.verify()
+
+  });
+});
+
