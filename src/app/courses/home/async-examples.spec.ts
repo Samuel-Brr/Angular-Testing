fe6144df3dd0225444e2cb4096c7068275d97087
Samuel-Brr <samuel.barre972@gmail.com>
2022-05-04 04:40:27 +0200

    Testing Promised-based code - Introduction to Microtasks

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -20,4 +20,42 @@ fdescribe('Some examples', () => {
 
   }))
 
+  fit('Asyn test examp - plain Promise', () => {
+
+    let test = false;
+
+    console.log("Creating promise");
+
+    setTimeout(() => {
+
+      console.log('setTimeout() first callback trigered');
+
+    });
+
+    setTimeout(() => {
+
+      console.log('setTimeout() second callback trigered');
+
+    });
+
+    Promise.resolve().then(() => {
+
+      console.log('Promise first then() evaluated successfully');
+
+      return Promise.resolve();
+    })
+      .then(() => {
+
+        console.log('Promise second then() evaluated successfully');
+
+        test = true
+
+      })
+
+    console.log('Running test assertions')
+
+    expect(test).toBeTruthy();
+
+  })
+
 } )
